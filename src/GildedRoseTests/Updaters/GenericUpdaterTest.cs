0c222b9c2659489812fb598d8265d15708e8bc28
jerrod wetherill <jerrod.wetherill@siraxis.co.uk>
2025-02-27 17:29:10 +0000

    Added more updater tests

--- src/GildedRoseTests/Updaters/GenericUpdaterTest.cs
+++ src/GildedRoseTests/Updaters/GenericUpdaterTest.cs
@@ -103,6 +103,30 @@ namespace GildedRoseTests.Updaters
             Assert.Equal(expectedSellin, item.SellIn);
         }
 
+        [Fact]
+        public void Past_Sellby_Date_And_Quality_Is_0_Quality_Does_Not_Go_Negative()
+        {
+            //Arrange
+            var expectedQuality = 0;
+            var expectedSellin = -2;
+
+            var item = new Item()
+            {
+                Name = "test Item",
+                Quality = 0,
+                SellIn = -1
+            };
+
+            IUpdater genericUpdate = new GenericItemUpdater(item);
+
+            //Act
+            var result = genericUpdate.UpdateItem();
+
+            //Assert
+            Assert.Equal(expectedQuality, item.Quality);
+            Assert.Equal(expectedSellin, item.SellIn);
+        }
+
     }
 
     public class BrieUpdaterTest
@@ -230,7 +254,6 @@ namespace GildedRoseTests.Updaters
 
     }
 
-
     public class BackStagePassUpdaterTest
     {
 
@@ -259,7 +282,31 @@ namespace GildedRoseTests.Updaters
         }
 
         [Fact]
-        public void Sellby_Date_Less_Than_10_Days_But_Greater_Than_5_Cut_Off_Increases_Quality_By_2()
+        public void Sellby_Date_Greater_Than_10_Days_Quality_Is_50_Quality_Does_Not_Increase()
+        {
+            //Arrange
+            var expectedQuality = 50;
+            var expectedSellin = 11;
+
+            var item = new Item()
+            {
+                Name = "test Item",
+                Quality = 50,
+                SellIn = 12
+            };
+
+            IUpdater genericUpdate = new BackStagePassItemUpdater(item);
+
+            //Act
+            var result = genericUpdate.UpdateItem();
+
+            //Assert
+            Assert.Equal(expectedQuality, item.Quality);
+            Assert.Equal(expectedSellin, item.SellIn);
+        }
+
+        [Fact]
+        public void Sellby_Date_Less_Than_10_Days_But_Greater_Than_5_Increases_Quality_By_2()
         {
             //Arrange
             var expectedQuality = 12;
@@ -283,7 +330,31 @@ namespace GildedRoseTests.Updaters
         }
 
         [Fact]
-        public void Sellby_Date_Less_Than_5_Days_But_Greater_Than_0_Cut_Off_Increases_Quality_By_3()
+        public void Sellby_Date_Less_Than_10_Days_But_Greater_Than_5_Quality_Is_49_Quality_Set_To_50()
+        {
+            //Arrange
+            var expectedQuality = 50;
+            var expectedSellin = 9;
+
+            var item = new Item()
+            {
+                Name = "test Item",
+                Quality = 49,
+                SellIn = 10
+            };
+
+            IUpdater genericUpdate = new BackStagePassItemUpdater(item);
+
+            //Act
+            var result = genericUpdate.UpdateItem();
+
+            //Assert
+            Assert.Equal(expectedQuality, item.Quality);
+            Assert.Equal(expectedSellin, item.SellIn);
+        }
+
+        [Fact]
+        public void Sellby_Date_Less_Than_5_Days_But_Greater_Than_0_Increases_Quality_By_3()
         {
             //Arrange
             var expectedQuality = 13;
@@ -293,6 +364,30 @@ namespace GildedRoseTests.Updaters
             {
                 Name = "test Item",
                 Quality = 10,
+                SellIn = 1
+            };
+
+            IUpdater genericUpdate = new BackStagePassItemUpdater(item);
+
+            //Act
+            var result = genericUpdate.UpdateItem();
+
+            //Assert
+            Assert.Equal(expectedQuality, item.Quality);
+            Assert.Equal(expectedSellin, item.SellIn);
+        }
+
+        [Fact]
+        public void Sellby_Date_Less_Than_5_Days_But_Greater_Than_0_Quality_Is_49_Quality_Set_To_50()
+        {
+            //Arrange
+            var expectedQuality = 50;
+            var expectedSellin = 0;
+
+            var item = new Item()
+            {
+                Name = "test Item",
+                Quality = 49,
                 SellIn = 1
             };
 
@@ -442,6 +537,54 @@ namespace GildedRoseTests.Updaters
             {
                 Name = "test Item",
                 Quality = 3,
+                SellIn = -1
+            };
+
+            IUpdater genericUpdate = new ConjuredItemUpdater(item);
+
+            //Act
+            var result = genericUpdate.UpdateItem();
+
+            //Assert
+            Assert.Equal(expectedQuality, item.Quality);
+            Assert.Equal(expectedSellin, item.SellIn);
+        }
+
+        [Fact]
+        public void Past_Sellby_Date_And_Quality_Is_2_Quality_Does_Not_Go_Negative()
+        {
+            //Arrange
+            var expectedQuality = 0;
+            var expectedSellin = -2;
+
+            var item = new Item()
+            {
+                Name = "test Item",
+                Quality = 2,
+                SellIn = -1
+            };
+
+            IUpdater genericUpdate = new ConjuredItemUpdater(item);
+
+            //Act
+            var result = genericUpdate.UpdateItem();
+
+            //Assert
+            Assert.Equal(expectedQuality, item.Quality);
+            Assert.Equal(expectedSellin, item.SellIn);
+        }
+
+        [Fact]
+        public void Past_Sellby_Date_And_Quality_Is_1_Quality_Does_Not_Go_Negative()
+        {
+            //Arrange
+            var expectedQuality = 0;
+            var expectedSellin = -2;
+
+            var item = new Item()
+            {
+                Name = "test Item",
+                Quality = 1,
                 SellIn = -1
             };
 
